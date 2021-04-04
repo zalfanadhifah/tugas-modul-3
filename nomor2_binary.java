dd78a08685060275d0bb967cfdf3f800fdadc76a
zalfanadhifah <80451797+zalfanadhifah@users.noreply.github.com>
2021-04-04 18:01:33 +0700

    Add files via upload

---
+++ nomor2_binary.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul3_tugas;
+
+/**
+ *
+ * @author ASUS
+ */
+public class nomor2_binary {
+    
+}
